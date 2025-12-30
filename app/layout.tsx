8dff31d8bea364cb2c0d0ed4cbba45a8b88fa36b
Md Abdur Rahman Palash <abdurrahmanpalashbd@gmail.com>
2025-12-30 19:23:08 +0600

    tabbar fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,16 @@ import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import "../styles/index.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NeuByte",
+  icons: {
+    icon: "/images/logo/NEUNYTE11.png",
+  },
+};
 
 export default function RootLayout({
   children,
